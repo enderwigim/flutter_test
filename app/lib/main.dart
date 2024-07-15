325dae046e0f5d5206023cae58e8750bb27098a0
Santiago Lorenzano <enderwigim@gmail.com>
2024-07-15 09:41:10 +0200

    Image added and changes between login and register made

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,15 +1,32 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
+} 
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  bool login = true;
+
+  void tooglePage() {
+    setState(() {
+      login = !login;
+    });
+  }
+
+
+  @override
   Widget build(BuildContext context) {
+  
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -37,63 +54,46 @@ class MainApp extends StatelessWidget {
                             width: textFieldWidth,
                             height: textFieldHeigh,
                             padding: const EdgeInsetsDirectional.symmetric(horizontal: 30.0, vertical: 20.0),
-                            color: Colors.grey[300],
+                            color: Colors.grey[200],
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.center,
                               mainAxisAlignment: MainAxisAlignment.spaceAround,
                               children: [
-                                  const Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
+                                  login? LoginPage() : RegisterPage(),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                                     children: [
-                                      const TextField(
-                                        decoration: InputDecoration(
-                                                    border: OutlineInputBorder(),
-                                                    hintText: "User"
+                                      ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                            padding: EdgeInsets.symmetric(horizontal: 35.00, vertical: 20.0), // Increase button size
+                                            textStyle: const TextStyle(fontSize: 20), // Increase font size
                                         ),
+                                        onPressed: tooglePage, 
+                                        child: const Text("Login")
                                       ),
-                                      
-                                      const SizedBox(height: 20.0),
-                                      
-                                      const TextField(
-                                        decoration: InputDecoration(
-                                                    border: OutlineInputBorder(),
-                                                    hintText: "Password",)
+                                      ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                            padding: EdgeInsets.symmetric(horizontal: 35.00, vertical: 20.0), // Increase button size
+                                            textStyle: const TextStyle(fontSize: 20), // Increase font size
+                                        ),
+                                        onPressed: tooglePage, 
+                                        child: const Text("Register")
                                       ),
                                     ],
-                                  ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                  children: [
-                                    ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                          padding: EdgeInsets.symmetric(horizontal: 35.00, vertical: 20.0), // Increase button size
-                                          textStyle: const TextStyle(fontSize: 20), // Increase font size
-                                      ),
-                                      onPressed: () {}, 
-                                      child: const Text("Login")
-                                    ),
-                                    ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                          padding: EdgeInsets.symmetric(horizontal: 35.00, vertical: 20.0), // Increase button size
-                                          textStyle: const TextStyle(fontSize: 20), // Increase font size
-                                      ),
-                                      onPressed: () {}, 
-                                      child: const Text("Register")
-                                    ),
-                                  ],
-                                )
-                              ],),
-                          )
-                        ],
-                      ),
+                                  )
+                                ],),
+                            )
+                          ],
+                        ),
                       Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
                           Container(
                             width: imageWidth,
-                            child: Image(
-                              image: NetworkImage("https://cdn.pixabay.com/photo/2018/03/26/14/07/space-3262811_1280.jpg")),
+                            child: Image.asset(
+                              ("../assets/img/integra.PNG"),
+                              fit: BoxFit.fill),
                           )
                         ],),
                     ],
@@ -111,3 +111,94 @@ class MainApp extends StatelessWidget {
     );
   }
 }
+
+
+class LoginPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          RichText(
+            text: const TextSpan(
+              text: 'Integra',
+              style: TextStyle(
+                fontSize: 30.00
+                ),
+              children: [
+                TextSpan(
+                  text: 'QS',
+                  style: TextStyle(
+                    fontSize: 30.00
+                  ),
+                )
+              ]
+            )),
+          const SizedBox(height: 100.0),
+          const TextField(
+            decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "User"
+            ),
+          ),
+          
+          const SizedBox(height: 20.0),
+          
+          const TextField(
+            decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Password",)
+          ),
+        ],
+      );
+  }
+}
+
+class RegisterPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          RichText(
+            text: const TextSpan(
+              text: 'Integra',
+              style: TextStyle(
+                fontSize: 30.00
+                ),
+              children: [
+                TextSpan(
+                  text: 'QS',
+                  style: TextStyle(
+                    fontSize: 30.00
+                  ),
+                )
+              ]
+            )),
+          const SizedBox(height: 100.0),
+          const TextField(
+            decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "User"
+            ),
+          ),
+          
+          const SizedBox(height: 20.0),
+          
+          const TextField(
+            decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Password",)
+          ),
+          const SizedBox(height: 20.0),
+          
+          const TextField(
+            decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Confirm Password",)
+          ),
+        ],
+      );
+  }
+}
+
